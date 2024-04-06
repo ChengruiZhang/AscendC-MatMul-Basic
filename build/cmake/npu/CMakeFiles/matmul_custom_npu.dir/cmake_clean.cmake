8d12e678c73146c08199dc99fbe0de12b8186ada
Ray <zcr897507954@gmail.com>
2024-04-06 21:49:44 +0800

    matmul

---
+++ build/cmake/npu/CMakeFiles/matmul_custom_npu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/code/samples8_0/samples/cplusplus/level1_single_api/4_op_dev/6_ascendc_custom_op/kernel_invocation/MatMul/matmul_custom_npu"
+  "/code/samples8_0/samples/cplusplus/level1_single_api/4_op_dev/6_ascendc_custom_op/kernel_invocation/MatMul/matmul_custom_npu.pdb"
+  "CMakeFiles/matmul_custom_npu.dir/__/__/main.cpp.o"
+  "CMakeFiles/matmul_custom_npu.dir/__/__/matmul_custom.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CCE)
+  include(CMakeFiles/matmul_custom_npu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ verification.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import numpy as np
+
+loss = 1e-3
+minimum = 10e-10
+
+def verify_result(real_result, golden):
+    real_result = np.fromfile(real_result, dtype=np.float32)
+    golden = np.fromfile(golden, dtype=np.float32)
+    print(real_result)
+    print(golden)
+    result = np.abs(real_result - golden)
+    deno = np.maximum(np.abs(real_result), np.abs(golden))
+    result_atol = np.less_equal(result, loss)
+    result_rtol = np.less_equal(result / np.add(deno, minimum), loss)
+    if not result_rtol.all() and not result_atol.all():
+        if np.sum(result_rtol == False) > real_result.size * loss and np.sum(result_atol == False) > real_result.size * loss:
+            print("[ERROR] result error")
+            return False
+    print("test pass")
+    return True
+
+def verify():
+    result = np.fromfile("./output/B.bin", dtype=np.float16)
+    print(result)
+    result = np.fromfile("./input/x2_gm.bin", dtype=np.float16)
+    print(result)
+
+if __name__ == '__main__':
+    verify_result(sys.argv[1],sys.argv[2])
+    verify()
